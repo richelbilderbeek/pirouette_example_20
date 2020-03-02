2494f716309a43bf92b70fabd12ccede586fb8cc
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 14:04:26 +0100

    Clearer and shorter code

--- example_20.R
+++ example_20.R
@@ -13,6 +13,7 @@ n_replicates <- 5
 n_taxa <- c(10, 20, 30, 40)
 crown_age <- 10
 is_testing <- is_on_travis()
+
 # Number of replicates per number of taxa
 if (is_testing) {
   n_replicates <- 2
@@ -34,6 +35,7 @@ for (i in seq_along(rng_seeds)) {
   )
 }
 expect_equal(length(phylogenies), length(rng_seeds))
+
 # Create pirouette parameter sets
 pir_paramses <- create_std_pir_paramses(
   n = length(phylogenies)
@@ -42,18 +44,7 @@ expect_equal(length(pir_paramses), length(phylogenies))
 if (is_testing) {
   pir_paramses <- shorten_pir_paramses(pir_paramses)
 }
-# Save tree to files
-for (i in seq_along(pir_paramses)) {
-  expect_equal(length(pir_paramses), length(phylogenies))
-  rng_seed <- pir_paramses[[i]]$alignment_params$rng_seed
-  folder_name <- file.path(paste0("example_", example_no, "_", rng_seed))
-  # Create folder, do not warn if it already exists
-  dir.create(folder_name, showWarnings = FALSE, recursive = TRUE)
-  ape::write.tree(
-    phylogenies[[i]],
-    file = file.path(folder_name, "true_tree.newick")
-  )
-}
+
 # Do the runs
 pir_outs <- pir_runs(
   phylogenies = phylogenies,
@@ -62,14 +53,10 @@ pir_outs <- pir_runs(
 
 # Save
 for (i in seq_along(pir_outs)) {
-  expect_equal(length(pir_paramses), length(pir_outs))
-  rng_seed <- pir_paramses[[i]]$alignment_params$rng_seed
-  folder_name <- file.path(paste0("example_", example_no, "_", rng_seed))
   pir_save(
     phylogeny = phylogenies[[i]],
     pir_params = pir_paramses[[i]],
     pir_out = pir_outs[[i]],
-    folder_name = folder_name
+    folder_name = dirname(pir_paramses[[i]]$alignment_params$fasta_filename)
   )
 }
-
