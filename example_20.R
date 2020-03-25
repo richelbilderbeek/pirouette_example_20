f1fca36ca25deb0fa581f6f80d28bfaf988cc110
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-25 19:57:59 +0100

    Use 5 replicates (from 2), will take 40 hours (from 16)

--- example_20.R
+++ example_20.R
@@ -10,7 +10,7 @@ library(testthat)
 # Constants
 is_testing <- is_on_ci()
 example_no <- 20
-n_replicates <- 2
+n_replicates <- 5
 n_taxa <- c(5, 10, 20, 30)
 crown_age <- 10
 folder_name <- paste0("example_", example_no)
